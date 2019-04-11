e14afbd82b23d94755de3e0427caed90900e024c
Sophie <sophie.jacquot@le-campus_numerique.fr>
2019-04-11 16:03:11 +0200

    exo5

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,1 +1,52 @@
 console.log("exercice 4");
+$(document).ready(() => {
+    // 1. si l'utilisateur clique sur le bouton 'signup' alors lancer une alerte pour lui dire : "Merci nous vous tiendrons informé des différentes offres";
+    $(".btn-danger").click(function () {
+        alert('Merci nous vous tiendrons informé des différentes offres');
+    });
+
+    // 2. récupérer maintenant le contenu de l'input pour adapter votre message : "Merci `adresse email rentrée`, nous vous tiendrons informé des différentes offres`;
+
+    $(".btn-danger").click(function () {
+        var mail = $("[type='email']").val()
+        alert('Merci ' + mail + ' nous vous tiendrons informé des différentes offres');
+    });
+
+    // 3. si l'utilisateur double clique sur l'onglet 'Produits' alors cacher l'onglet 'Produits';
+    $('.navbar-nav li:eq(1)').dblclick(function () {
+        $(this).hide()
+    });
+
+    // 4. si l'utilisateur clique sur une image alors changer le bouton 'Cart' en haut à droite pour marquer le fait qu'un produit a été ajouté au panier. S'il clique deux fois on doit avoir le texte Cart(2);
+    var counter = 0;
+
+    $("img").click(function () {
+        counter++;
+        $('#myNavbar > ul.nav.navbar-nav.navbar-right > li:nth-child(2) > a > span').text(counter);
+
+    });
+
+    // 5. un site ecommerce traque la position de la souris pour savoir si vous êtes intéréssés par un produit. Affichez dans la console 'L'utilisateur regarde ' + le nom du produit si la souris est au dessus d'une image de produit. Vous pouvez ajouter des ids sur les images dans la page HTML. *Bonus*: faire de même sans utiliser les ids;
+
+    $("img").on('mouseenter', function () {
+        var imageName = $("img").attr("src");
+        console.log("L'utilisateur regarde " + imageName);
+    });
+
+
+    // 6. maintenant, traquer si un utilisateur est en train de remplir le champ pour la newsletter. Comment récupérer le fait que l'utilisateur vient de cliquer dans le champ pour rentrer son adresse et comment récupérer le fait que l'utilisateur a tapé quelque chose dedans ?;
+        $("[type='email']").on('click', function(){
+            console.log("L'utilisateur a cliqué sur le champ de la newsletter");
+        });
+
+        $("[type='email']").on('keypress', function(){
+            console.log("L'utilisateur remplit le champ pour la newsletter");
+        });
+
+
+    // 5. *Bonus ++* : récupérer le nombre de produits que l'utilisateur a achetés. Par exemple si l'utilisateur clique sur l'image ipad alors 'Cart' doit être remplacé par 'Cart(25)' puisque l'offre correspond à 25 ipads...
+
+    
+
+
+});
--- exo-5/script.js
+++ exo-5/script.js
@@ -1,1 +1,103 @@
+$(document).ready(() => {
+
 console.log("exercice 5");
+
+var computerChoice = getComputerChoice(0,2)
+console.log(computerChoice);
+
+
+function getUserChoice(userInput) {
+    userInput = userInput.toLowerCase();
+    //console.log(userInput);
+    if ((userInput === 'rock') || (userInput === 'paper') || (userInput === 'scissors') || (userInput === 'bomb')) {
+        return userInput;
+    } else {
+        console.log('erreur');
+        return 1;
+    }
+}
+
+function getComputerChoice(min, max) {
+    computerChoice = Math.floor(Math.random() * (max - min + 1)) + min;
+    //console.log(computerChoice);
+    if (computerChoice === 0) {
+        return 'rock';
+    } else if (computerChoice === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
+function determineWinner(userChoice, computerChoice) {
+
+    if (userChoice === computerChoice) {
+        return 'tied';
+    }
+
+    else if (userChoice === 'rock' && computerChoice === 'scissors') {
+        return 'Won';
+    }
+
+    else if (userChoice === 'paper' && computerChoice === 'rock') {
+        return 'Won';
+    }
+
+    else if (userChoice === 'scissors' && computerChoice === 'paper') {
+        return 'Won';
+    }
+
+    else if (userChoice === 'bomb') {
+        return 'Won';
+    }
+
+    else {
+        return 'Lost';
+    }
+}
+
+// function playGame(){
+//     let uChoice = getUserChoice('bomb');    
+//     console.log(uChoice);
+
+    
+
+//     let resultat = determineWinner(uChoice, computerChoice);
+//     console.log(resultat);
+// }
+
+// playGame();  
+
+    var imageName;
+    
+
+    // 3. l'utilisateur va cliquer sur une image afin de faire son choix. En utilisant des sélecteurs JQuery, faites en sorte que lorsque l'utilisateur clique sur une image, son choix s'affiche dans la console.
+    $(".container img").on('click', function () {
+        imageName = $(this).attr("id");
+        console.log("The user chose " + imageName);
+    });
+
+
+
+    // 4. maintenant créez une div avec l'id userChoice, et remplacez le contenu de cette div par le choix de l'utilisateur;
+
+    $(".container img").on('click', function () {
+
+        $("#userChoice").text("You chose" + " " + imageName);
+
+    });
+
+    // 5. créez une div avec l'id computerChoice et y mettre dedans le choix de l'ordinateur que vous avez obtenu suite à l'appel de la fonction getComputerChoice créée dans le cours précédent;
+    $(".btn-primary").on('click', function () {
+        $("#computerChoice").text("The computer chose" + " " + computerChoice);
+    });
+
+
+
+
+    // 6. ajoutez une div pour afficher le résultat, le texte doit être vert si vous avez gagné, gris en cas d'égalité et rouge si vous avez perdu.
+    $(".btn-dark").on('click', function () {
+        $("#resultat").text(determineWinner(imageName, computerChoice));
+    });
+
+});
